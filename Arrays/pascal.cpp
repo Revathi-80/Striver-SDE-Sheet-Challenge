ac57ac0621f2875eaa1958938268a6e5e6e8f9a7
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-06-04 20:39:29 +0530

    Update pascal.cpp

--- Arrays/pascal.cpp
+++ Arrays/pascal.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h> 
 vector<vector<long long int>> printPascal(int n) 
-{ vector<vector<int>>ans(n);
+{ vector<vector<long long int>>ans(n);
   for(int i=0;i<n;i++){
-      ans[i]=vector<int>(i+1,1);
+      ans[i]=vector<long long int>(i+1,1);
       for(int j=1;j<i;j++){
           ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
       }
