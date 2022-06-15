cd0f4926a50aadca494dc37d52568df4fac1f75a
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-06-15 23:24:24 +0530

    Create rotate_image.cpp

---
+++ Arrays/rotate_image.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        transpose(matrix);
+        ref(matrix);
+    }
+    void transpose(vector<vector<int>>&matrix){
+        int n=matrix.size();
+        for(int i=0;i<(n);i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[j][i];
+                matrix[j][i]=matrix[i][j];
+                matrix[i][j]=temp;
+            }
+        }
+    }
+    void ref(vector<vector<int>>&matrix){
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                int temp=matrix[i][n-j-1];
+                matrix[i][n-j-1]=matrix[i][j];
+                matrix[i][j]=temp;
+            }
+        }
+    }
+    
+    
+};
